409dc4bc3d33b1cee2a2e9a47316420f28a41406
olegussawa <olegussawa3@gmail.com>
2022-12-28 22:21:54 +0700

    add aapend text

--- file.php
+++ file.php
@@ -10,11 +10,11 @@ interface iFile
 		public function getExt();  // расширение файла
 		public function getSize(); // размер файла
 		
-		/*public function getText();          // получает текст файла
+		public function getText();          // получает текст файла
 		public function setText($text);     // устанавливает текст файла
 		public function appendText($text);  // добавляет текст в конец файла
 		
-		public function copy($copyPath);    // копирует файл
+		/*public function copy($copyPath);    // копирует файл
 		public function delete();           // удаляет файл
 		public function rename($newName);   // переименовывает файл
 		public function replace($newPath);  // перемещает файл*/
@@ -22,8 +22,10 @@ interface iFile
 	
 	
 	class File implements iFile{
+		
 		private $Pathfile;
 		public $arr;
+		
 		public function __construct($Pathfile){
 			
 		$this->Pathfile=$Pathfile;
@@ -46,11 +48,37 @@ interface iFile
 			//расширение
 			return $this->arr['extension'];}
 			
-			public function getSize(){
+			  public function getSize(){
 				 // размер
-			return filesize ($this->Pathfile);}
+			  return filesize ($this->Pathfile);}
+			
+			
+			   public function getText(){
+				 // читает содержимое
+				 if (file_exists($this->Pathfile)) {
+				 	return file_get_contents($this->Pathfile);      }
+				 	return 'нет такого файла';
 			
 	}
+	
+	            public function setText($text){
+		
+				 // записывает содержимое
+				file_put_contents($this->Pathfile, $text); 
+				}
+				
+				public function appendText($text){
+					
+					// добавляет текст в конец файла
+	            file_put_contents($this->Pathfile, file_get_contents($this->Pathfile).' '. $text); 
+				}
+				
+				
+	}
+	
+	
+	
+	
 	
 	
 	
