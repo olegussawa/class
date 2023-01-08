ed72cdc75590fd92896e408fc1b9e5e0c342482d
olegussawa <olegussawa3@gmail.com>
2023-01-09 03:25:43 +0700

    add css method

---
+++ addcss.php
@@ -0,0 +1,92 @@
+class Tag
+	{
+		private $name;
+		private $arr=[];
+		
+		public function __construct($name)
+		{
+			$this->name = $name;
+			
+		}
+		
+		public function addClass($classname){
+			
+			if(isset($this->arr['class'])){
+				
+			$arrclass=explode(' ',$this->arr['class']);
+			
+			if(!in_array($classname,$arrclass)){$arrclass[]=$classname; $this->arr['class']=implode(' ',$arrclass);}
+			
+			
+			}else
+			
+			
+			$this->arr['class']=$classname;
+			
+			return $this;
+		}
+		
+		public function setAtr($name,$val){
+			$this->arr[$name]=$val;
+			return $this;
+			
+		}
+		
+		public function setAtrs($attrs)
+	{
+		foreach ($attrs as $name => $val) {
+			$this->setAtr($name, $val);
+		}
+		
+		return $this;
+	}
+		
+		public function removeAtr($name){
+			unset ($this->arr[$name]);
+			return $this;
+			
+		}
+		
+	// Выводим открывающую часть тега:
+		public function open()
+		{
+			$name = $this->name;
+			$atrib=$this->getatr();
+			return "<$name $atrib>";
+		}
+		// Выводим закрывающую часть тега:
+		public function close()
+		{
+			$name = $this->name;
+			return "</$name>";
+		}
+		
+		
+		private function getatr(){
+			if(!empty($this->arr)){
+				$res='';
+				foreach($this->arr as $key=>$el){$res.=$key.'='."\"$el\"";
+				}return $res;
+			}return '';
+	}
+	}
+	
+	
+	
+	
+			
+			
+		
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+?>
